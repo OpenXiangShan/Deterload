f3d8116192829fd82831adddbda02175f073a02d
xieby1 <xieby1@outlook.com>
2024-12-30 14:17:35 +0800

    builders: imgBuilder: linux: enable overlayfs build

--- builders/imgBuilder/linux/default.nix
+++ builders/imgBuilder/linux/default.nix
@@ -2,13 +2,29 @@
 , bc
 , flex
 , bison
+# * With OverlayFS disabled, the default `unpackPhase` will copy the entire `common-build`,
+#   which involves approximately 1.7GB of disk writes.
+#   When building N linux images simultaneously, the disk write throughput becomes N*1.7GB.
+# * With OverlayFS enabled, the build processes are overlaied onto 1.7GB `common-build`,
+#   resulting in minimal disk writes.
+#   However, because of the nix sandbox, overlayfs cannot be used directly in nix build processes,
+#   we utilize `runInLinuxVM` to work around this limitation.
+#   (For more details, see https://discourse.nixos.org/t/using-fuse-inside-nix-derivation/8534.)
+#   * If your nixbld* users have access to /dev/kvm,
+#     there will be no noticable performance degradation.
+#   * If your nixbld* users lack access to /dev/kvm,
+#     QEMU will fall back to translation mode (TCG),
+#     which is approximately 100 times slower.
+, enableOverlayFS ? true
+, fuse-overlayfs
+, vmTools
 
 , riscv64-cc
 , rmExt
 , initramfs
   # TODO: use overlayfs to reduce disk usage
 , common-build
-}@args: stdenv.mkDerivation {
+}@args: let overlayfsDisabled = stdenv.mkDerivation {
   name = "${rmExt initramfs.name}.linux";
   src = common-build;
   buildInputs = [
@@ -33,7 +49,21 @@
     make -j $NIX_BUILD_CORES
   '';
   installPhase = ''
+    # runInLinuxVM will auto create dir $out
+    rm -rf $out
     cp arch/riscv/boot/Image $out
   '';
   passthru = args;
-}
+};
+overlayfsEnabled = vmTools.runInLinuxVM (overlayfsDisabled.overrideAttrs (old: {
+  unpackPhase = ''
+    mkdir workdir
+    mkdir upperdir
+    mkdir overlaydir
+    /run/modprobe fuse
+    ${fuse-overlayfs}/bin/fuse-overlayfs -o lowerdir=${old.src},workdir=workdir,upperdir=upperdir overlaydir
+    cd overlaydir
+  '';
+  memSize = 1024;
+}));
+in if enableOverlayFS then overlayfsEnabled else overlayfsDisabled
