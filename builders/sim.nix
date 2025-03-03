b960607c7e178b45f9d4f1e3f3bc3490f0179771
xieby1 <xieby1@outlook.com>
2025-03-03 11:25:34 +0800

    builders: sim: enable h

--- builders/sim.nix
+++ builders/sim.nix
@@ -11,7 +11,7 @@
   "-nographic"
   "-m 8G"
   "-smp ${smp}"
-  "-cpu rv64,v=true,vlen=128,h=false,sv39=true,sv48=false,sv57=false,sv64=false"
+  "-cpu rv64,v=true,vlen=128,h=true,sv39=true,sv48=false,sv57=false,sv64=false"
   # "-plugin ${qemu}/lib/libprofiling.so,workload=${workload_name},intervals=${intervals},target=$out"
   "-icount shift=0,align=off,sleep=on"
 ])
